5ca4ac446885c12228425384939722c48e411b84
Ding Dong <tomynguy@gmail.com>
2023-06-17 10:59:51 -0700

    UI elements and whatnot

--- public/client.js
+++ public/client.js
@@ -1,1 +1,14 @@
 const socket = io();
+
+let username = '';
+
+function createHTML() {
+    var input = Object.assign(document.createElement("input"), {className: "input", value: "Username", onfocus: function() { if (this.value === "Username") { this.value = ""; } }});
+    var button = Object.assign(document.createElement("button"), {className: "send", textContent: "Send"});
+    var div = document.createElement("div");
+    div.className = "center-bottom";
+    div.append(input, button);
+    document.body.append(div);
+  }
+  
+  createHTML();
