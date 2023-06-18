a36c8d351641f115e0fad5d8a6b64dcf0a6df17a
Ding Dong <tomynguy@gmail.com>
2023-06-18 14:56:09 -0700

    Improved message filtering

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io =  require('socket.io')(http);
+const fs = require('fs');
 
 const userPrefix = "User";
 let PORT = 3000;
 let users = new Set();
-let invalidWords = new Set(['']);
+const invalidWords = getInvalidWords();
+
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
@@ -20,7 +22,7 @@ io.on('connection', (socket) => {
     socket.on('login', (username) => {
       if (users.has(username)) {
         socket.emit("loginFail", "Username Taken");
-      } else if (invalidWords.has(username)){
+      } else if (stringFilter(username)){
         socket.emit("loginFail", "Invalid Username");
       } else {
         console.log(username + ' logged in');
@@ -35,7 +37,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('messageSent', (user, message) => {
-      if (invalidWords.has(message)) return;
+      if (stringFilter(message)) return;
       const newMessage = user + " : " + message;
       console.log(newMessage)
       io.to("room").emit("messageRecieved", newMessage);
@@ -52,4 +54,20 @@ function randUser(username, recurse) {
   for (let i = 0; i < (5 + recurse/5); i++) username += Math.floor(Math.random() * 10);
   if (users.has(username)) return randUser(userPrefix, recurse + 1);
   return username;
+}
+
+// Filters out naughty words...
+function stringFilter(string) {
+  let filtered_string = string.replace(/[^a-zA-Z0-9]/g, '');
+  for (const word in invalidWords) {
+    if (filtered_string.includes(invalidWords[word])) return true;
+  }
+  return string.replace(/\s+/g, '').length == 0;
+}
+
+function getInvalidWords() {
+  const csvFilePath = 'src/invalidWords.csv';
+  const csvContent = fs.readFileSync(csvFilePath, 'utf-8');
+  const csvLines = csvContent.split('\n').map(line => line.trim());
+  return Array.from(csvLines);
 }
