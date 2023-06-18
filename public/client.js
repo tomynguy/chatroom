ed7d276f5c81eead5c66b7921f44604c37f7ea55
Ding Dong <tomynguy@gmail.com>
2023-06-17 18:11:47 -0700

    Added login Functionality

--- public/client.js
+++ public/client.js
@@ -1,14 +1,52 @@
 const socket = io();
 
-let username = '';
+let usernameInput, usernameButton, title, center, center_top, chatInput, chatButton, center_bottom;
 
-function createHTML() {
-    var input = Object.assign(document.createElement("input"), {className: "input", value: "Username", onfocus: function() { if (this.value === "Username") { this.value = ""; } }});
-    var button = Object.assign(document.createElement("button"), {className: "send", textContent: "Send"});
-    var div = document.createElement("div");
-    div.className = "center-bottom";
-    div.append(input, button);
-    document.body.append(div);
-  }
-  
-  createHTML();
+socket.on('getUser', (username) => {
+    // Initialize all html elements
+    usernameInput = Object.assign(document.createElement('input'), {className: 'input', value: username, onfocus: function() { if (this.value === username) { this.value = ''; username = '';} }});
+    usernameButton = Object.assign(document.createElement('button'), {className: 'send', textContent: 'Send'});
+    title = Object.assign(document.createElement('h1'), {textContent: '~Tuktopia~'});
+    center = document.createElement('div');
+    center_top = document.createElement('div');
+    chatInput = Object.assign(document.createElement('input'), {className: 'input', value: 'Send message...', onfocus: function() { if (this.value === 'Send message...') { this.value = ''; } }});
+    chatButton = Object.assign(document.createElement('button'), {className: 'send', textContent: 'Send'});
+    center_bottom = document.createElement('div');
+
+    // Set Div Classes
+    center.className = 'center';
+    center_top.className = 'center-top';
+    center_bottom.className = 'center-bottom';
+
+    // Append elements to their respective divs
+    center.append(usernameInput, usernameButton);
+    center_top.append(title);
+    center_bottom.append(chatInput, chatButton);
+
+    // Add elements to doc and add button functionality
+    document.body.append(center, center_top);
+    usernameButton.onclick = login;
+    usernameInput.addEventListener('keypress', function(event) {
+        if (event.key == 'Enter') {
+            event.preventDefault();
+            usernameButton.click();
+        }
+    });
+});
+
+function login() {
+    socket.emit('login', usernameInput.value);
+}
+
+socket.on('loginFail', () => {
+    usernameInput.value = 'Username Taken';
+});
+
+socket.on('loginSuccess', () => {
+    usernameInput.remove();
+    usernameButton.remove();
+    title.remove();
+    center.remove();
+    center_top.remove();
+    document.body.append(center_bottom);
+});
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const http = require('http').Server(app);
 const io =  require('socket.io')(http);
 
+const userPrefix = "User";
 let PORT = 3000;
+let users = new Set();
 
 app.use(express.static('public'));
 
@@ -13,7 +15,18 @@ app.get('/', (req,res) => {
 
 io.on('connection', (socket) => {
     console.log('A user connected');
-  
+    socket.emit("getUser", randUser());
+
+    socket.on('login', (username) => {
+      if (users.has(username)) {
+        socket.emit("loginFail");
+      } else {
+        console.log(username + ' logged in');
+        users.add(username);
+        socket.emit("loginSuccess");
+      }
+    });
+
     socket.on('disconnect', () => {
       console.log('A user disconnected');
     });
@@ -22,3 +35,15 @@ io.on('connection', (socket) => {
 http.listen(PORT, () => {
     console.log('Server listening on http://localhost:' + PORT);
 });
+
+function randUser() {
+  let username = userPrefix;
+  for (let i = 0; i < 5; i++) username += Math.floor(Math.random() * 10);
+
+  if (users.has(username)) {
+    username = userPrefix;
+    return randUser();
+  }
+
+  return username;
+}
