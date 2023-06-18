7ddae78a5b80dfafbdaa33b16c2c1df84ed9e146
Ding Dong <tomynguy@gmail.com>
2023-06-18 13:51:47 -0700

    Added messages, Visual Improvements, Code Quality, Error cases, Banned Words/Usernames, Randomly generated unique usernames, Foundation for chat rooms

--- public/client.js
+++ public/client.js
@@ -1,35 +1,41 @@
 const socket = io();
 
-let usernameInput, usernameButton, title, center, center_top, chatInput, chatButton, center_bottom;
+let usernameInput, usernameButton, title, center, center_top, chatInput, chatButton, center_bottom, user, chatbox;
 
 socket.on('getUser', (username) => {
     // Initialize all html elements
-    usernameInput = Object.assign(document.createElement('input'), {className: 'input', value: username, onfocus: function() { if (this.value === username) { this.value = ''; username = '';} }});
+    usernameInput = Object.assign(document.createElement('input'), {className: 'username', value: username, onfocus: function() { if (this.value === username) { this.value = ''; username = '';} }});
     usernameButton = Object.assign(document.createElement('button'), {className: 'send', textContent: 'Send'});
-    title = Object.assign(document.createElement('h1'), {textContent: '~Tuktopia~'});
-    center = document.createElement('div');
-    center_top = document.createElement('div');
-    chatInput = Object.assign(document.createElement('input'), {className: 'input', value: 'Send message...', onfocus: function() { if (this.value === 'Send message...') { this.value = ''; } }});
-    chatButton = Object.assign(document.createElement('button'), {className: 'send', textContent: 'Send'});
-    center_bottom = document.createElement('div');
+    usernameInvalid = Object.assign(document.createElement('label'), {className: 'invalid'});
+    title = Object.assign(document.createElement('h1'), {className: 'title', textContent: '~Tuktopia~'});
+    center = Object.assign(document.createElement('div'), {className: 'center'});
+    center_top = Object.assign(document.createElement('div'), {className: 'center_top'});
 
-    // Set Div Classes
-    center.className = 'center';
-    center_top.className = 'center-top';
-    center_bottom.className = 'center-bottom';
+    chatInput = Object.assign(document.createElement('input'), {className: 'chat', placeholder: 'Send Message...'});
+    chatButton = Object.assign(document.createElement('button'), {className: 'send', textContent: 'Send'});
+    center_bottom = Object.assign(document.createElement('div'), {className: 'center_bottom'});
+    chatbox = Object.assign(document.createElement('div'), {className: 'chatbox'});
 
     // Append elements to their respective divs
-    center.append(usernameInput, usernameButton);
+    center.append(usernameInput, usernameButton, usernameInvalid);
     center_top.append(title);
     center_bottom.append(chatInput, chatButton);
 
     // Add elements to doc and add button functionality
     document.body.append(center, center_top);
     usernameButton.onclick = login;
-    usernameInput.addEventListener('keypress', function(event) {
-        if (event.key == 'Enter') {
+    usernameInput.addEventListener('keydown', function(event) {
+        if (event.key == 'Enter' && !event.repeat) {
             event.preventDefault();
             usernameButton.click();
+        }
+    });
+
+    chatButton.onclick = messageSend;
+    chatInput.addEventListener('keydown', function(event) {
+        if (event.key == 'Enter' && !event.repeat) {
+            event.preventDefault();
+            chatButton.click();
         }
     });
 });
@@ -38,15 +44,27 @@ function login() {
     socket.emit('login', usernameInput.value);
 }
 
-socket.on('loginFail', () => {
-    usernameInput.value = 'Username Taken';
+function messageSend() {
+    socket.emit("messageSent", user, chatInput.value);
+    chatInput.value = '';
+}
+
+socket.on('loginFail', (errmsg) => {
+    usernameInput.value = '';
+    usernameInvalid.innerHTML = errmsg;
+    usernameInvalid.style.display = 'block';
 });
 
-socket.on('loginSuccess', () => {
-    usernameInput.remove();
-    usernameButton.remove();
-    title.remove();
+socket.on('loginSuccess', (username) => {
     center.remove();
     center_top.remove();
-    document.body.append(center_bottom);
+    document.body.append(center_bottom, chatbox);
+    user = username;
+});
+
+socket.on('messageRecieved', (message) => {
+    let m = Object.assign(document.createElement('p'), {className: 'message', textContent: message});
+    chatbox.append(m);
+    console.log(message);
+    m.scrollIntoView();
 });
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const io =  require('socket.io')(http);
 const userPrefix = "User";
 let PORT = 3000;
 let users = new Set();
-
+let invalidWords = new Set(['']);
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
@@ -15,35 +15,41 @@ app.get('/', (req,res) => {
 
 io.on('connection', (socket) => {
     console.log('A user connected');
-    socket.emit("getUser", randUser());
+    socket.emit("getUser", randUser(userPrefix, 0));
 
     socket.on('login', (username) => {
       if (users.has(username)) {
-        socket.emit("loginFail");
+        socket.emit("loginFail", "Username Taken");
+      } else if (invalidWords.has(username)){
+        socket.emit("loginFail", "Invalid Username");
       } else {
         console.log(username + ' logged in');
         users.add(username);
-        socket.emit("loginSuccess");
+        socket.emit("loginSuccess", username);
+        socket.join("room");
       }
     });
 
     socket.on('disconnect', () => {
       console.log('A user disconnected');
     });
+
+    socket.on('messageSent', (user, message) => {
+      if (invalidWords.has(message)) return;
+      const newMessage = user + " : " + message;
+      console.log(newMessage)
+      io.to("room").emit("messageRecieved", newMessage);
+    });
+
   });
 
 http.listen(PORT, () => {
     console.log('Server listening on http://localhost:' + PORT);
 });
 
-function randUser() {
-  let username = userPrefix;
-  for (let i = 0; i < 5; i++) username += Math.floor(Math.random() * 10);
-
-  if (users.has(username)) {
-    username = userPrefix;
-    return randUser();
-  }
-
+// Find a unique random name. If random name isn't found after 5 tries, append another digit.
+function randUser(username, recurse) {
+  for (let i = 0; i < (5 + recurse/5); i++) username += Math.floor(Math.random() * 10);
+  if (users.has(username)) return randUser(userPrefix, recurse + 1);
   return username;
 }
